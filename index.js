a0164a63d6d30b00f88e61c485552d885bb13553
Shamsher <shamsher@geekyants.com>
2022-12-22 11:47:06 +0530

    first commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import messaging from '@react-native-firebase/messaging';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
     console.log('Message handled in the background!', remoteMessage);
+    
   });
 
 AppRegistry.registerComponent(appName, () => App);
